98547b2131a8bd45010877e420c0eb58bf9f01cf
One234Fi <54912519+One234Fi@users.noreply.github.com>
2022-05-03 16:12:41 -0500

    Update Player.java
    * added a toString method
    * added a dieSelection method that will later be used to select which dice to roll during a user's turn
    * added a roll method to roll selected dice and store them in an array
    * added beginRoll(), which currently does nothing, but will be used to handle differences between the initial roll and the partial rolls a user can do on their turn
    * added a getScore() method to allow other classes read-only access to a Player's score
    * changed how the scanner sc is initialized for a player, each player has a new scanner instead of one scanner being passed through every constructor, this might be reverted later
    * added the onTheBoard boolean for future use in determining valid actions allowed on a turn

--- GreedDiceGame/Player.java
+++ GreedDiceGame/Player.java
@@ -9,23 +9,31 @@ import java.util.*;
 class Player {
     private int score = 0;
     private Die[] dice;
-    Scanner sc;
+    //more flexibility in your rolling is available once you get on the board by scoring 1000 points 
+    private boolean onTheBoard;
     
-    public Player (int handSize, Scanner sc) {
+    Scanner sc = new Scanner(System.in);
+    
+    public Player (int handSize) {
         dice = new Die[handSize];
         for (int i = 0; i < handSize; i++) {
             dice[i] = new Die(6);
         }
-        this.sc = sc;
+        onTheBoard = false;
     }
     
     void addToScore (int val) {
         score += val;
     }
     
+    public int getScore() {
+        return score;
+    }
+    
     //Prompt the user to take an action
     public void startTurn() {
-        System.out.println("Type Roll or Pass...");
+        //System.out.println("Turn start! Your score is: " + score);
+        System.out.println("Type \"Roll\" or \"Pass\" to start your turn...");
         parseAction(sc.next());
     }
     
@@ -33,13 +41,17 @@ class Player {
     void parseAction(String s) {
         
         switch (s.toLowerCase()) {
-            case "pass": pass(); break;
+            case "pass": /*pass();*/ break;
             case "roll": roll(); break;
             
             
             default:    System.out.println("Invalid command!");
                         startTurn();
         }
+    }
+    
+    void beginRoll() {
+        
     }
     
     //roll all the dice and print the results
@@ -53,5 +65,42 @@ class Player {
             System.out.print(results[i] + ", ");
         }
         System.out.println("");
+    }
+    
+    int[] roll(int diceToRoll) {
+        int [] results = new int[dice.length];
+        for (int i = 0; i < dice.length; i++) {
+            results[i] = dice[i].roll();
+        }
+        return results;
+    }
+    
+    //get an integer from the user to indicate which dice to select for rolling
+    int dieSelection() {
+        System.out.println("Please indicate which dice to select by inputting a sequence of 1s and 0s, "
+                + "\n1s for selected dice and 0s for not selected dice. Example: 101100 to select the 1st, 3rd, and 4th dice");
+        String attemptedSelection = sc.next();
+        boolean validSelection = false;
+        while (!validSelection) {
+            if (attemptedSelection.length() > 6 && attemptedSelection.matches("[0-1]+")) {
+                validSelection = true;
+                int selection = Integer.parseInt(attemptedSelection);
+                return selection;
+            }
+            else {
+                System.out.println("Invalid selection, please make sure your selection is 6 digits long and only contains 1s and 0s...");
+                attemptedSelection = sc.next();
+            }
+        }
+        
+        return 0;
+    }
+    
+    String[] validResults = {"1", "5", "222", "333", "444", "666", "123456"};
+    
+    @Override
+    public String toString() {
+        String s = "Human Player:\t\tScore = " + score;
+        return s;
     }
 }
